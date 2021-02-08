56bdd584bb61b107cf3526957b00365030c8e13e
Juan Jose de la Torre <juanjdlt@gmail.com>
2021-02-08 15:59:16 -0500

    Cameoparison checkpoint1  - Welcome Screen -

---
+++ cameoparison/src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+  import {onMount} from 'svelte';
+  import Welcome from './screens/Welcome.svelte';
+  import {select} from './select';
+
+  let celebs_promise;
+
+	let state = 'welcome'; //or 'playing'
+  let selection;
+
+  const start = async (e) => {
+    const {celebs, lookup} = await celebs_promise;
+    console.log("resolved?", celebs);
+    selection = select(celebs, lookup, e.detail.category.slug);
+    state = 'playing';
+  };
+
+  const load_celebs = async () => {
+    const res = await fetch('https://cameo-explorer.netlify.app/celebs.json');
+    const data = await res.json();
+
+    const lookup = new Map();
+    data.forEach(c => {
+      lookup.set(c.id, c)
+    });
+
+    const subset = new Set();
+    data.forEach(c => {
+      if (c.reviews >= 50) {
+        subset.add(c);
+        c.similar.forEach(id => {
+          subset.add(lookup.get(id));
+        });
+      }
+    });
+
+    return {
+      celebs: Array.from(subset),
+      lookup
+    }
+
+  }
+
+  onMount(() => {
+    celebs_promise = load_celebs();
+    console.log("CelebsPromise", celebs_promise);
+  });
+</script>
+
+<main>
+  {#if state === 'welcome'}
+    <Welcome on:select={start}/>
+  {:else if state === 'playing'}
+    <p>game screen goes here</p>
+  {/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+    max-width: 800px;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+	}
+</style>
